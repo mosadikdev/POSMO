95b4f4e4299e2022ae033a03ddfa2b1e3dd23264
mosadikdev <mohamedsadik.dev@gmail.com>
2026-02-15 19:29:50 +0000

    Improve POS grid layout (fixed product card size)

--- pos-frontend/src/pages/POS.jsx
+++ pos-frontend/src/pages/POS.jsx
@@ -1,35 +1,59 @@
 export default function POS() {
-  return (
-    <div className="h-screen grid grid-cols-3">
-      
-      {/* Products */}
-      <div className="col-span-2 p-4 grid grid-cols-4 gap-4 bg-gray-100">
-        <div className="bg-white p-4 shadow rounded">Perfume</div>
-        <div className="bg-white p-4 shadow rounded">Oil</div>
-        <div className="bg-white p-4 shadow rounded">Bottle</div>
-      </div>
+    return (
 
-      {/* Cart */}
-      <div className="bg-white p-4 border-l">
-        <h2 className="text-xl font-bold mb-4">Cart</h2>
+        <div className="h-screen grid grid-cols-3">
 
-        <div className="flex justify-between">
-          <span>Perfume</span>
-          <span>50 DH</span>
-        </div>
-
-        <div className="mt-6 border-t pt-4">
-          <div className="flex justify-between text-lg font-bold">
-            <span>Total</span>
-            <span>50 DH</span>
-          </div>
-
-          <button className="mt-4 w-full bg-green-500 text-white py-2 rounded">
-            Pay
-          </button>
-        </div>
-      </div>
-
+            <div className="col-span-2 p-4 bg-gray-100">
+  
+  <div className="grid grid-cols-6 gap-6 auto-rows-[140px]">
+    
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Perfums
     </div>
-  )
+
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Oils
+    </div>
+
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Flacons
+    </div>
+
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Perfums
+    </div>
+
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Flacons
+    </div>
+
+    <div className="bg-white shadow rounded flex items-center justify-center text-lg font-semibold">
+      Perfums
+    </div>
+  </div>
+
+</div>
+
+            <div className="m-4">
+                    <h2 className="text-xl font-bold mb-4">Cart</h2>
+
+                    <div className="flex justify-between mb-4">
+                        <span>Perfums</span>
+                        <span>50 DH</span>
+                    </div>
+
+
+                    <div className="flex justify-between border-t font-bold">
+                        <span>Total: </span>
+                        <span>50 DH</span>
+                    </div>
+
+
+                    <button className="w-full bg-green-500 py-2 rounded shadow mt-4 text-white">Pay</button>
+
+            </div>
+
+        </div>
+
+    )
 }
